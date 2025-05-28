2051d2445b5aa4f8894476245276b69cbf633dac
Mr.T <945119403@qq.com>
2025-05-28 18:18:38 +0800

    add shadcn-ui

--- src/components/request-select.tsx
+++ src/components/request-select.tsx
@@ -1,8 +1,15 @@
-"use client";
-import { RequestMethod } from "@/lib/constants";
-import { useState, useEffect } from "react";
-import { Select, SelectTrigger, SelectContent, SelectItem } from "./ui/select";
-import { ChevronDownIcon } from "lucide-react";
+'use client';
+import { RequestMethod } from '@/lib/constants';
+import { useState, useEffect } from 'react';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+import { ChevronDownIcon } from 'lucide-react';
 
 interface RequestSelectorProps {
   value: string;
@@ -27,13 +34,25 @@ export const RequestSelector = ({ value, onChange }: RequestSelectorProps) => {
   };
 
   return (
+    // <Select value={selectedMethod} defaultValue={defaultMethod} onValueChange={handleModelChange}>
+    //   <SelectTrigger className="relative inline-flex w-32 h-8 border-0 border-input text-[13px] font-medium items-center gap-[5px] cursor-pointer rounded-l-sm bg-[#f9fafb] px-4 leading-none text-violet-950 outline-none data-[placeholder]:text-violet-800">
+    //     {selectedMethod}
+    //     <ChevronDownIcon size={16} className="absolute right-2 top-1/2 -translate-y-1/2" />
+    //   </SelectTrigger>
+    //   <SelectContent>
+    //     {RequestMethod.map((item) => (
+    //       <SelectItem key={item.value} value={item.value}>
+    //         {item.label}
+    //       </SelectItem>
+    //     ))}
+    //   </SelectContent>
+    // </Select>
     <Select value={selectedMethod} defaultValue={defaultMethod} onValueChange={handleModelChange}>
-      <SelectTrigger className="relative inline-flex w-32 h-8 border-0 border-input text-[13px] font-medium items-center gap-[5px] cursor-pointer rounded-l-sm bg-[#f9fafb] px-4 leading-none text-violet-950 outline-none data-[placeholder]:text-violet-800">
-        {selectedMethod}
-        <ChevronDownIcon size={16} className="absolute right-2 top-1/2 -translate-y-1/2" />
+      <SelectTrigger className="w-32">
+        <SelectValue placeholder="Method" />
       </SelectTrigger>
       <SelectContent>
-        {RequestMethod.map(item => (
+        {RequestMethod.map((item) => (
           <SelectItem key={item.value} value={item.value}>
             {item.label}
           </SelectItem>
